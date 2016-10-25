630d248905cab8e2d55a4f09698067523daa4050
Eirik Aabøe <eirikaaboe@gmail.com>
2016-10-25 16:09:15 +0200

    Some changes

--- js/map.js
+++ js/map.js
@@ -4,32 +4,20 @@ var Kartverket = new L.TileLayer.Kartverket("topo2graatone");
 Kartverket.addTo(map);
 
 
-var myStyle = {"color": "#ff0000", "weigth": 0, "opacity": 1, "fillOpacity": 1};
+var myStyle = {"color": "#000000", "weigth": 0, "opacity": 1, "fillOpacity": 1};
 var Norge = L.geoJson.ajax("data/fylker.geojson", {style:myStyle});
-Norge.addTo(map);
+
+// Norge.addTo(map);
 
 
-// navigator.geolocation.getCurrengetCurrentPosition(function(position){
-//   var lat = position.coords.latutude;
-//   var lon = position.coords.longitude;
-// });
-//
-// alert(lat);
-// lc = L.control.locate().addTo(map);
-//
-// lc.start();
-//
-// console.log(lc);
-// // alert(this._event.latlng());
-// alert(pos.coords.latitude);
-// leaflet mini map
+
 
 navigator.geolocation.getCurrentPosition(onSuccess,
                                          onError,
                                          {maximumAge: 10000, timeout: 5000, enableHighAccuracy: true,
                                          interval: 6000})
 
-function onSuccess(pos){
+  function onSuccess(pos){
   var lat = pos.coords.latitude;
   var lon = pos.coords.longitude;
   console.log(lat, lon);
@@ -42,18 +30,29 @@ function onSuccess(pos){
         "coordinates": [lon, lat]
     }
 };
-    // console.log([lon,lat].toGeoJSON());
-    var buffer = turf.buffer(point,500,'meters');
+    var bufferStyle = {"color": "#ff0000"};
+    var buffer = turf.buffer(point,5000,'meters');
     console.log(buffer);
     console.log(Norge);
-    L.geoJson(buffer).addTo(map);
+    var geojsonbuffer= L.geoJson(buffer, {style:bufferStyle});
+    geojsonbuffer.addTo(map);
+    console.log(geojsonbuffer);
+    JSON.stringify(Norge);
+    console.log(Norge);
+    var layers = L.layerGroup([Kartverket, Norge, geojsonbuffer]);
+    // layers.addTo(map);
+    // geojsonbuffer.addTo(map);
     // var bufferLayer = L.GeoJSON(buffer).addTo(map);
     // buffer.setGeoJSON();
     // console.log(turf.featurecollection);
     // var result = turf.featurecollection([buffer.features, point]);
 
-}
+    // var differenced = turf.difference(geojsonbuffer, Norge)
+    // differenced.addTo(map);
 
+}
 function onError(){
   alert('Noe gikk feil');
 }
+
+// TODO: Add leaflet mini map
